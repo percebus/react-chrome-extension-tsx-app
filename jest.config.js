b46916999a7032f9980b8eaffb45d88711781a9f
JC Guerrero <JCystems@gMail.com>
2022-07-21 09:26:52 -0500

    feat(test reports)

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,10 @@ module.exports = {
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: [
     '<rootDir>/src/setupTests.ts'
+  ],
+  reporters: [
+    'default',
+    // 'github-actions', // FIXME
+    'jest-junit'
   ]
 }
