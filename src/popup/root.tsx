2f2f8abba8fe6ea45d3a5d2cebcee7bd6062c97a
JC Guerrero <JCystems@gMail.com>
2022-07-28 09:16:00 -0500

    refactor(root.css) aqs index.scss

--- src/popup/root.tsx
+++ src/popup/root.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './root.css'
 import App from './app/App'
 
 const divHTMLElement = document.getElementById('root') as HTMLElement
